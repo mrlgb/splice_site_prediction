0d1c085beb17df00ee8822adc61647c6166caa29
uyoung-jeong <farandole09@unist.ac.kr>
2019-06-03 16:52:31 +0900

    test session can run, but processing time is too slow

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 import numpy as np
 from datetime import datetime
 import os
+from tqdm import tqdm
 
 from data_loader import Dataset
 from config import get_args
@@ -37,7 +38,7 @@ x_valid = torch.from_numpy(x_valid).type(torch.FloatTensor)
 y_valid = torch.from_numpy(y_valid).type(torch.LongTensor)
 
 testset = torch.utils.data.TensorDataset(x_valid, y_valid)
-testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
 # load our model
 print('loading model')
@@ -64,76 +65,105 @@ model.to(device)
 correct = 0
 total = 0
 with torch.no_grad():
+	pbar = tqdm(total=len(y_valid)/batch_size, desc='computing validation set score')
 	for data in testloader:
 		inputs, labels = data[0].unsqueeze(1).to(device), data[1].to(device)
 		outputs = model(inputs)
 		_, predicted = torch.max(outputs.data, 1)
 		total += labels.size(0)
 		correct += (predicted == labels).sum().item()
+		pbar.update(1)
+	pbar.close()
 
 print('Accuracy of the network on the test sequences: %f' % (100.0 * float(correct) / float(total)))
+
+del model
 
 # DSSP test
 correct = 0
 total = 0
 
-as_dssp = model_from_json(open(os.path.join(os.path.dirname(__file__), 'AS_model.json')).read())
-as_dssp.load_weights(os.path.join(os.path.dirname(__file__), 'DS_model.hdf5'))
+as_dssp = model_from_json(open(os.path.join(os.getcwd(), 'DSSP', 'AS_model.json')).read())
+as_dssp.load_weights(os.path.join(os.getcwd(), 'DSSP', 'DS_model.hdf5'))
 
-ds_dssp = model_from_json(open(os.path.join(os.path.dirname(__file__), 'AS_model.json')).read())
-ds_dssp.load_weights(os.path.join(os.path.dirname(__file__), 'DS_model.hdf5'))
+ds_dssp = model_from_json(open(os.path.join(os.getcwd(), 'DSSP', 'AS_model.json')).read())
+ds_dssp.load_weights(os.path.join(os.getcwd(), 'DSSP', 'DS_model.hdf5'))
 
 # s: pytorch tensor with shape (10,15)
 def convert_to_dssp_format(s):
-	s = s.data.numpy()
 	s = s.flatten()
+	s = s.astype(int)
 
 	# decode
+	seq_types = []
+	input_vecs = []
 	seq = ''
 	i=0
-	while i < 140:
-		token = ''.join(s[i:i+6])
-		if token=='100000':
-			seq += 'A'
-		elif token=='010000':
-			seq += 'C'
-		elif token=='001000':
-			seq += 'G'
-		elif token=='000100':
-			seq += 'T'
-		elif token=='000010':
-			seq += 'N'
-		elif token=='000001':
-			seq += 'H'
+	j=0
+	while i < batch_size:
+		while j < 140 * 6:
+			token = ''.join(map(str,s[i*140*6+j:i*140*6+j+6]))
+			if token=='100000':
+				seq += 'A'
+			elif token=='010000':
+				seq += 'C'
+			elif token=='001000':
+				seq += 'G'
+			elif token=='000100':
+				seq += 'T'
+			elif token=='000010':
+				seq += 'N'
+			elif token=='000001':
+				seq += 'H'
+			j += 6
 
-	if len(seq) != 140:
-		print('Failed to convert to DSSP input format')
-		exit(1)
-	seq_type = None
-	if seq[68:70] == 'AG':
-		seq_type = 'AS'
-	if seq[70:72] == 'GT':
-		seq_type == 'DS'
+		if len(seq) != 140:
+			print('Failed to convert to DSSP input format')
+			import ipdb; ipdb.set_trace()
+			exit(1)
+		seq_type = ''
+		if seq[68:70] == 'AG':
+			seq_type = 'AS'
+		elif seq[70:72] == 'GT':
+			seq_type = 'DS'
+		else:
+			print('Failed to figure out seq type')
+			import ipdb; ipdb.set_trace()
+			exit()
+		input_vec = dataset.one_hot_dssp(seq)
+		
+		seq_types.append(seq_type)
+		input_vecs.append(input_vec)
+		i += 1
+	return seq_types, input_vecs
 
-	input_vec = dataset.one_hot_dssp(seq)
-	return seq_type, input_vec
+correct=0
+total=0
 
 with torch.no_grad():
+	pbar = tqdm(total=len(y_valid), desc='computing DSSP score')
 	for data in testloader:
 		# check donor/acceptor
-		seq_type, inputs = convert_to_dssp_format(data[0])
-        labels = data[1].data.numpy()
-		if seq_type == 'AS': # AS case
-			model = as_dssp
-		elif seq_type == 'DS': # DS case
-			model = ds_dssp
+		seq_type, inputs = convert_to_dssp_format(data[0].data.numpy()) # get batch_size of inputs
+		labels = data[1].data.numpy()
 
-		predict = model.predict(inputs, batch_size=1, verbose=0)[0,0]
-		predicted = 1 if predicted>0.5 else 0
-		total += labels.size(0)
-		correct += (predicted == labels).sum().item()
+		for t,x,y in zip(seq_type, inputs, labels): # iterate over batch_size
+			dssp = None
+			if t == 'AS': # AS case
+				dssp = as_dssp
+			elif t == 'DS': # DS case
+				dssp = ds_dssp
+			else:
+				print('ERROR: cannot allocate dssp model')
+				import ipdb; ipdb.set_trace()
+				exit(1)
+			predict = dssp.predict(x, batch_size=1, verbose=0)[0,0]
+			predicted = 1 if predict>0.5 else 0
+			total += 1
+			correct += (predicted == y)
+			pbar.update(1)
+	pbar.close()
 
 print('Accuracy of the DSSP on the test sequences: %f' % (100.0 * float(correct) / float(total)))
 
-ipdb.set_trace()
 print('Finished Execution')
